7b8cadee184ec4c2f255c16bcfc7b9902cec0134
brunorgdsantos <brunorgdsantos@gmail.com>
2025-06-08 15:04:49 -0300

    updates class 1

---
+++ aula1/main.go
@@ -0,0 +1,11 @@
+package main /*Com exceção do pagote main, todos os outros arquivos segue
+o mesmo nome de package igual ao diretorio em que eles estão incluidos. */
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Hello World\n")
+
+	println(a) /*Como o arquivo teste.go esta no mesmo pacote/diretorio que main.go,
+	tudo que esta no teste.go pode ser usando no main.go*/
+}
