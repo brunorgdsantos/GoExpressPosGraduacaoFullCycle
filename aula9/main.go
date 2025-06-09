2e3e49c724f03933a3958ab7627e1dfb84703a15
brunorgdsantos <brunorgdsantos@gmail.com>
2025-06-09 13:31:59 -0300

    updates class 9

--- aula9/main.go
+++ aula9/main.go
@@ -7,6 +7,8 @@ import (
 func main() {
 	valor := sum(1, 2, 5, 4)
 	fmt.Printf("O Valor é: %d\n", valor)
+
+	fmt.Println(sum(4, 5))
 }
 
 func sum(numeros ...int) int { //Isso é uma Função Variádica
