7b8d8e76b31eb67d02a0255fdb221edc080b77ab
brunorgdsantos <brunorgdsantos@gmail.com>
2025-06-09 13:53:29 -0300

    updates class 9

--- aula9/main.go
+++ aula9/main.go
@@ -9,6 +9,13 @@ func main() {
 	fmt.Printf("O Valor é: %d\n", valor)
 
 	fmt.Println(sum(4, 5))
+
+	funcaoAnonima := func() int { //No GO é possível ter funções anonimas, basta não definir um nome para ela.
+		//É possível ter funções dentro de funções
+		return sum(3, 9) * 2
+	}()
+
+	fmt.Printf("O valor é da Função Anônima: %d\n ", funcaoAnonima)
 }
 
 func sum(numeros ...int) int { //Isso é uma Função Variádica
