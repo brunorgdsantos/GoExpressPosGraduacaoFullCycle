1785e4bb0b1599b710d98f32d709e3ac6da68273
brunorgdsantos <brunorgdsantos@gmail.com>
2025-06-10 23:32:42 -0300

    updates class 13

---
+++ aula13/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func soma(a, b int) int {
+	a = 50
+	return a + b
+}
+
+func somaUsandoPonteiro(a, b *int) int {
+	return *a + *b
+}
+
+func main() {
+	minhaVar1 := 10
+	minhaVar2 := 20
+	fmt.Println(minhaVar1, minhaVar2)
+	fmt.Println(soma(minhaVar1, minhaVar2))
+
+	fmt.Println(somaUsandoPonteiro(&minhaVar1, &minhaVar2))
+}
