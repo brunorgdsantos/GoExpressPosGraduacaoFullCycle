f2267056d0330909cbbdc9c64327142b032f6619
brunorgdsantos <brunorgdsantos@gmail.com>
2025-06-14 22:41:18 -0300

    updates class 18

--- aula18/main.go
+++ aula18/main.go
@@ -8,4 +8,13 @@ import (
 func main() {
 	sm := matematica.Soma(1, 2)
 	fmt.Printf("O resultado é: %v\n", sm)
+
+	fmt.Println(matematica.A) //Consigo acessar pq A esta com maiusculo
+	//fmt.Println(matematica.a) //Não consigo acessar pq a esta minusculo
+
+	teste := matematica.Carro{
+		Ano:   "2025",
+		Preco: 125.5,
+	}
+	fmt.Printf("O resultado é: %v e %v\n", teste.Ano, teste.Preco)
 }
--- aula18/matematica/matematica.go
+++ aula18/matematica/matematica.go
@@ -1,5 +1,14 @@
 package matematica
 
-func Soma[T int | float64](a, b T) T {
+type Carro struct { //A struc Carro esta sendo exportada pq esta com maiusculo
+	marca string //Porém o atributo marca não esta sendo exportado
+	Ano   string //Os atributos Ano e Preco estão sendo exportados pq estão com letras maiusculas
+	Preco float64
+}
+
+func Soma[T int | float64](a, b T) T { //Em Go, quando uma func começa com letra maiuscula, significa que ela pode ser exportada e usada em outros arquivos.go
 	return a + b
 }
+
+var A int = 10 //consigo exportar pq esta com letra maiúscula
+var a int = 10 //consigo exportar pq esta com letra maiúscula
