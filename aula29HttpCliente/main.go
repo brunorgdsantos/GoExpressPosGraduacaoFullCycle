43090d8ff7051dbb433646afb48ac75c3fcc35dd
brunorgdsantos <brunorgdsantos@gmail.com>
2025-06-20 23:17:30 -0300

    updates class 29 - http request

---
+++ aula29HttpCliente/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func main() {
+	c := http.Client{Timeout: time.Microsecond} //Tempo limite de espera de resposta
+	resp, err := c.Get("http://google.com")
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close() //Sera executado por ultimo
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	println(string(body))
+}
