9b4e2111458ced4787ab72cc6a842309d3984374
brunorgdsantos <brunorgdsantos@gmail.com>
2025-06-21 23:16:38 -0300

    update class 31 - data insert db

--- aula31BancoDeDados/main.go
+++ aula31BancoDeDados/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+	"log"
+)
 
 type Product struct {
 	ID    string
@@ -17,5 +22,30 @@ func NewProduct(name string, price float64) *Product {
 }
 
 func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
+	produto := NewProduct("Notebook", 1240.0)
+	err = insertProduct(db, produto)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func insertProduct(db *sql.DB, product *Product) error { //Inserindo dados no Banco
+	stmt, err := db.Prepare("Insert into products(ID, Name,Price) values(?,?,?)") //Segurança contra SQL Injection
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.ID, product.Name, product.Price) //(product.ID, product.Name, product.Price) vão substituir as (?,?,?) da linha 35
+	if err != nil {
+		return err
+	}
+	return nil
 }
