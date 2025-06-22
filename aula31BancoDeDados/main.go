ff85c15616f4d690e0e8ea1abc8c3d0a372ced72
brunorgdsantos <brunorgdsantos@gmail.com>
2025-06-22 01:37:44 -0300

    update class 31 - read products database

--- aula31BancoDeDados/main.go
+++ aula31BancoDeDados/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
 	"log"
@@ -39,6 +40,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	p, err := selectProduct(db, produto.ID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Product: %v possui o preço de %2.f", p.Name, p.Price)
 }
 
 func insertProduct(db *sql.DB, product *Product) error { //Inserindo dados no Banco
@@ -71,17 +78,20 @@ func updateProduct(db *sql.DB, product *Product) error { //Atualizando dados do 
 	return nil
 }
 
-func selectOneProduct(db *sql.DB, id string) (*Product, error) { //Read Produtos, selecionando produtos
+func selectProduct(db *sql.DB, id string) (*Product, error) { //Read Produtos, selecionando produtos
 	stmt, err := db.Prepare("select id, name, price from products where id = ?") //Segurança contra SQL Injection
 	if err != nil {
-		return err, nil
+		return nil, err
 	}
 
 	defer stmt.Close()
 
-	_, err = stmt.Exec(id)
+	var p Product
+
+	err = stmt.QueryRow(id).Scan(&p.ID, &p.Name, &p.Price) //Faz as atribuições
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return nil
+
+	return &p, nil
 }
