3bb606975155633fd44b20dd27d9e1c462e9228a
brunorgdsantos <brunorgdsantos@gmail.com>
2025-06-28 14:35:00 -0300

    update class 32 - tests

--- aula32BancoDeDadosGORM/umParaUmHasOne/main.go
+++ aula32BancoDeDadosGORM/umParaUmHasOne/main.go
@@ -12,20 +12,20 @@ type Category struct {
 }
 
 type Product struct {
-	ID           int          `gorm:"primary_key,AUTO_INCREMENT"`
-	Name         string       `json:"name"`
-	Price        float64      `json:"price"`
-	CategoryID   int          `json:"category_id"` //Estamos relacionando duas tabelas
-	Category     Category     //Estamos relacionando duas tabelas
-	SerialNumber SerialNumber `gorm:"foreignKey:ProductID"` //Relacionando Product com SerialNumber
-	gorm.Model                                              //Cria algumas colunas a mais no banco
+	ID         int      `gorm:"primary_key,AUTO_INCREMENT"`
+	Name       string   //`json:"name"`
+	Price      float64  //`json:"price"`
+	CategoryID int      //`gorm:"foreignKey:CategoryID"` //Estamos relacionando duas tabelas
+	Category   Category //Estamos relacionando duas tabelas
+	//SerialNumber SerialNumber //`gorm:"foreignKey:ProductID"` //Relacionando Product com SerialNumber
+	gorm.Model //Cria algumas colunas a mais no banco
 }
 
-type SerialNumber struct {
+/*type SerialNumber struct {
 	ID        int `gorm:"primarykey"`
 	Number    string
 	ProductID int //Todos os SerialNumber terão um ProductId relacionados
-}
+}*/
 
 func main() {
 	dsn := "root:root@tcp(localhost:3306)/goexpert"
@@ -33,28 +33,28 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	db.AutoMigrate(&Category{}, &Product{}, &SerialNumber{}) //Cria as tabelas
+	db.AutoMigrate(&Category{}, &Product{} /*, &SerialNumber{}*/) //Cria as tabelas
 
-	Category := Category{ //Criando Categorias
-		Name: "Cama/Mesa",
+	category := Category{ //Criando Categorias
+		Name: "Cozinha",
 	}
-	db.Create(&Category)
+	db.Create(&category)
 
 	db.Create(&Product{ //Criando Produto
-		Name:       "Colchão",
-		Price:      1300.0,
+		Name:       "Fogão",
+		Price:      1500.0,
 		CategoryID: 1, //Passando o Id da Categoria
 	})
 
-	db.Create(&SerialNumber{ //Criado SerialNumber
+	/*db.Create(&SerialNumber{ //Criado SerialNumber
 		Number:    "123",
 		ProductID: 1,
-	})
+	})*/
 
 	var products []Product
 	db.Preload("Category").Preload("SerialNumber").Find(&products) //O Preload traz alguma informações a mais
 	for _, product := range products {
-		fmt.Println(product.Name, product.Category.Name, product.SerialNumber.Number)
+		fmt.Println(product.Name, product.Category.Name /*, product.SerialNumber.Number*/)
 	}
 
 }
