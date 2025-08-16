2ef3ce39e870f893313a7f511a76de07476fe0b6
brunorgdsantos <brunorgdsantos@gmail.com>
2025-08-16 19:39:57 -0300

    updates

--- aula33packaging/cmd/main.go
+++ aula33packaging/cmd/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/brunorgdsantos/GoExpressPosGraduacaoFullCycle/aula33packging/math"
 )
 
 func main() {
 	m := math.Math{A: 10, B: 20}
 	fmt.Println(m.Add())
+
+	n := math.NewMath(1, 2)
+	fmt.Println(n.Add2())
 }
