200da21ff6dff804d7f6d5cebf0bfb909d25c7ca
brunorgdsantos <brunorgdsantos@gmail.com>
2025-08-16 17:48:38 -0300

    upadtes

--- aula33packaging/cmd/main.go
+++ aula33packaging/cmd/main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/brunorgdsantos/GoExpressPosGraduacaoFullCycle/aula33packging/math
+	"github.com/brunorgdsantos/GoExpressPosGraduacaoFullCycle/aula33packging/math"
 )
 
 func main() {
-	fmt.Println("Hello World")
-
-	result := math.Add(1, 2)
-	fmt.Println("Resultado:", result)
+	m := math.Math{A: 10, B: 20}
+	fmt.Println(m.Add())
 }
